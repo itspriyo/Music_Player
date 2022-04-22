f6aef0242298daf74e05aa6151e4a9e6c34fe2be
P R I Y O <84788291+itspriyo@users.noreply.github.com>
2022-04-22 19:10:13 +0600

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -382,7 +382,7 @@ allcmd = ["play", "player", f"play@{Config.BOT_USERNAME}", f"player@{Config.BOT_
 
 @Client.on_message(filters.command(admincmds) & ~admin_filter & chat_filter)
 async def notforu(_, m: Message):
-    k = await _.send_cached_media(chat_id=m.chat.id, file_id="CAADBQADEgQAAtMJyFVJOe6-VqYVzAI", caption="You Are Not Authorized", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚡️Join Here', url='https://t.me/PremiumNetworkCommunity')]]))
+    k = await _.send_cached_media(chat_id=m.chat.id, file_id="CAADBQADEgQAAtMJyFVJOe6-VqYVzAI", caption="You Are Not Authorized", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚡️Join Here', url='https://t.me/Priyo_Mirror')]]))
     await delete_messages([m, k])
 
 @Client.on_message(filters.command(allcmd) & ~chat_filter & filters.group)
@@ -402,7 +402,7 @@ async def not_chat(_, m: Message):
         buttons = [
             [
                 InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/itspriyo/Music_Player'),
-                InlineKeyboardButton('🧩 Join Here', url='https://t.me/PremiumNetworkCommunity'),
+                InlineKeyboardButton('🧩 Join Here', url='https://t.me/Priyo_Mirror'),
             ]
             ]
         await m.reply("<b>You can't use this bot in this group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/itspriyo/Music_Player) below.</b>", disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(buttons))
